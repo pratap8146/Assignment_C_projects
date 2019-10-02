51d95c8fb97753a7cc3d71b9c7ab41e760568baf
Rish7223 <53928808+Rish7223@users.noreply.github.com>
2019-10-02 22:33:45 +0530

    Update table.c

--- table.c
+++ table.c
@@ -1,16 +1,18 @@
 // program to print table og given number.
+// these are comments plese do not remember it.
+
 #include <stdio.h>
 #include <conio.h>
 void main()
 {
     int n, table,i;
     clrscr();
-    printf("Enter any number: ");
+    printf("Enter any number: ");  //asking for user to input any number/
     scanf("%d",&n);
-    for(i=1;i<=10;i++)
+    for(i=1;i<=10;i++)      //loop to multiply number by 1to10 one by one 
     {
         table=n*i;
-        printf("%d*%d=%d\n",n,i,table);
+        printf("%d*%d=%d\n",n,i,table);  //display the table.
     }
     getch();
 }
